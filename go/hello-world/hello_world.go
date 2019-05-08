631ab2a9b0f8e91af0e916748c49669ac7927329
Spencer Owen <owenspencer@gmail.com>
2019-05-07 23:16:33 -0600

    exercism go

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// https://golang.org/doc/effective_go.html#commentary
+package greeting
+
+// HelloWorld should have a comment documenting it.
+func HelloWorld() string {
+	return "Hello, World!"
+}
