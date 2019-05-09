35e226b6b3c07a214717e1e14ce7d8cd57daf74f
Spencer Owen <owenspencer@gmail.com>
2019-05-08 22:40:16 -0600

    go fmt

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -5,62 +5,62 @@ import "sync"
 type Account struct {
 	balance int64
 	closed  bool
-  sync.Mutex
+	sync.Mutex
 }
 
-func Open(initialDeposit int64) *Account  {
-  if initialDeposit >= 0 {
-    a := new(Account)
-    a.balance = initialDeposit
-    a.closed = false
-    return a
-  } else {
-    return nil
-  }
+func Open(initialDeposit int64) *Account {
+	if initialDeposit >= 0 {
+		a := new(Account)
+		a.balance = initialDeposit
+		a.closed = false
+		return a
+	} else {
+		return nil
+	}
 
 }
 
-func (a *Account ) Balance() (balance int64, ok bool) {
-  if a.closed {
-    ok = false
-  } else {
-    ok = true
-  }
-  return a.balance, ok
+func (a *Account) Balance() (balance int64, ok bool) {
+	if a.closed {
+		ok = false
+	} else {
+		ok = true
+	}
+	return a.balance, ok
 }
 
 func (a *Account) Close() (payout int64, ok bool) {
-  a.Lock()
-  defer a.Unlock()
-  if a.closed {
-    ok = false
-  } else {
-    payout = a.balance
-    a.balance = 0
-    a.closed = true
-    ok = true
-  }
-  return payout, ok
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
+		ok = false
+	} else {
+		payout = a.balance
+		a.balance = 0
+		a.closed = true
+		ok = true
+	}
+	return payout, ok
 }
 
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-  a.Lock()
-  defer a.Unlock()
-  if a.closed == true {
-    ok = false
-  } else {
-    if amount < 0 {
-      if a.balance + amount < 0 {
-        ok = false
-      } else {
-        a.balance = a.balance + amount
-        ok = true
-      }
-    } else {
-      a.balance += amount
-      ok = true
-    }
+	a.Lock()
+	defer a.Unlock()
+	if a.closed == true {
+		ok = false
+	} else {
+		if amount < 0 {
+			if a.balance+amount < 0 {
+				ok = false
+			} else {
+				a.balance = a.balance + amount
+				ok = true
+			}
+		} else {
+			a.balance += amount
+			ok = true
+		}
 
-  }
-  return a.balance, ok
+	}
+	return a.balance, ok
 }
